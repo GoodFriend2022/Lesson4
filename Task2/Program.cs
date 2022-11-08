230bcec12e063f74a31073434414300f22c86b73
Uri_Klevcov <a-08-137.mail.ru>
2022-11-08 21:46:06 +0500

    T2 Sum Numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе. Реализовать через функции.
+
+int Prompt(string message)
+{
+    System.Console.Write(message + " > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number%10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите целое число");
+System.Console.WriteLine($"Сумма цифр в введенном Вами числе равна => {SumNumbers(number)}");
