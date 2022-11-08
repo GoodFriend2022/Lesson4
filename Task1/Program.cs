6bf977b693950268328aa40a3dfb30fcca73d11c
Uri_Klevcov <a-08-137.mail.ru>
2022-11-08 21:33:32 +0500

    T1 Number Power

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Реализовать через функции.
+
+int Prompt(string message)
+{
+    System.Console.Write(message + " > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberPower(int number, int power)
+{
+    if (power == 1) return number;
+    return number * NumberPower(number, power - 1);
+}
+
+int number = Prompt("Введите число, которое хотите возвести в степень");
+int power = Prompt("Введите степень, в которую хотите возвести число");
+System.Console.WriteLine($"{number} в степени {power} равно => {NumberPower(number, power)}");
+
+
+
+
+
